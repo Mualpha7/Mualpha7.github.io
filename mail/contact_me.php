949bae131c7e1a60f033ddc8ef9d0c44a2ab2628
Francisco Verón Ferreira <mualpha7@gmail.com>
2018-10-21 10:33:57 -0400

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,6 +20,6 @@ $header .= "Reply-To: $email";
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
 
-mail($to,$email_subject,$email_body,$headers);
+mail($to, $subject, $body, $header);
 return true;
 ?>
